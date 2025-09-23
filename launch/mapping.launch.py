f0d567a682bd8d8424d4ebb266c88eaf2e47842b
Jaerak Son <sjr9017@naver.com>
2025-09-24 08:21:34 +0900

    Update Lidar static tf

--- launch/mapping.launch.py
+++ launch/mapping.launch.py
@@ -47,7 +47,7 @@ def generate_launch_description():
         package='tf2_ros',
         executable='static_transform_publisher',
         arguments=[
-            '0.45', '0.4', '0.79',
+            '0.375', '0.125', '0.79',
             '1.5708', '0.0', '3.14159',
             'base_link',
             'livox_frame'
